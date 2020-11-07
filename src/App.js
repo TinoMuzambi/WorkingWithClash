5a957563021c36289733021e5df56d4585daefb6
Tino <tinomuzambi@gmail.com>
2020-11-07 17:51:27 +0200

    Refactoring code.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [query, setQuery] = useState("");
+	const [playerTag, setPlayerTag] = useState("");
 
-	const callAPI = async () => {
-		const URL = `http://localhost:5000/api/players/${query}`;
+	const getPlayerData = async () => {
+		const URL = `http://localhost:5000/api/players/${playerTag}`;
 		try {
 			const result = await fetch(URL);
 			const data = await result.json();
@@ -17,30 +17,28 @@ function App() {
 
 	return (
 		<div className="forms">
-			{[1, 1, 1, 1].map((key) => (
-				<div className="form-group" key={key}>
-					<div className="content">
-						<h3 className="title">Player Info</h3>
-						<form className="form">
-							<label htmlFor="request">Clash Player Tag</label>
-							<input
-								type="text"
-								name="request"
-								id="request"
-								className="tag"
-								value={query}
-								onChange={(e) => setQuery(e.target.value)}
-							/>
-							<input
-								type="button"
-								value="Send"
-								className="send"
-								onClick={callAPI}
-							/>
-						</form>
-					</div>
+			<div className="form-group">
+				<div className="content">
+					<h3 className="title">Player Info</h3>
+					<form className="form">
+						<label htmlFor="request">Clash Player Tag</label>
+						<input
+							type="text"
+							name="request"
+							id="request"
+							className="tag"
+							value={playerTag}
+							onChange={(e) => setPlayerTag(e.target.value)}
+						/>
+						<input
+							type="button"
+							value="Send"
+							className="send"
+							onClick={getPlayerData}
+						/>
+					</form>
 				</div>
-			))}
+			</div>
 		</div>
 	);
 }
