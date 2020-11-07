429948638e220228292b1a5723f59a9d090c0f19
Tino <tinomuzambi@gmail.com>
2020-11-07 18:37:14 +0200

    Cleanup.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import ReactJson from "react-json-view";
 import Card from "./components/Card";
 
 function App() {
-	const [clanTag, setClanTag] = useState("");
-	const [clanJson, setClanJson] = useState("");
-
 	return (
 		<div className="forms">
 			<Card content="player" />
