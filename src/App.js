799a63c00622cc6687e508c2c8354c8fa360dc45
Tino <tinomuzambi@gmail.com>
2020-11-07 18:19:41 +0200

    Adds clan info fetching.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ function App() {
 	const [playerTag, setPlayerTag] = useState("");
 	const [playerJson, setPlayerJson] = useState("");
 
+	const [clanTag, setClanTag] = useState("");
+	const [clanJson, setClanJson] = useState("");
+
 	const getPlayerData = async () => {
-		const URL = `http://localhost:5000/api/players/${playerTag}`;
+		const URL = `http://localhost:5000/api/player/${playerTag}`;
 		try {
 			const result = await fetch(URL);
 			const data = await result.json();
 			setPlayerJson(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	const getClanData = async () => {
+		const URL = `http://localhost:5000/api/clan/${clanTag}`;
+		try {
+			const result = await fetch(URL);
+			const data = await result.json();
+			setClanJson(data);
 		} catch (error) {
 			console.error(error);
 		}
@@ -23,11 +37,10 @@ function App() {
 				<div className="content">
 					<h3 className="title">Player Info</h3>
 					<form className="form">
-						<label htmlFor="request">Clash Player Tag</label>
+						<label htmlFor="request">Player Tag</label>
 						<input
 							type="text"
 							name="request"
-							id="request"
 							className="tag"
 							value={playerTag}
 							onChange={(e) => setPlayerTag(e.target.value)}
@@ -39,7 +52,46 @@ function App() {
 							onClick={getPlayerData}
 						/>
 					</form>
-					<ReactJson className="json" src={playerJson} />
+					{playerJson && (
+						<ReactJson
+							className="json"
+							src={playerJson}
+							enableClipboard={false}
+							displayDataTypes={false}
+							indentWidth={2}
+						/>
+					)}
+				</div>
+			</div>
+
+			<div className="form-group">
+				<div className="content">
+					<h3 className="title">Clan Info</h3>
+					<form className="form">
+						<label htmlFor="request">Clash Tag</label>
+						<input
+							type="text"
+							name="request"
+							className="tag"
+							value={clanTag}
+							onChange={(e) => setClanTag(e.target.value)}
+						/>
+						<input
+							type="button"
+							value="Send"
+							className="send"
+							onClick={getClanData}
+						/>
+					</form>
+					{clanJson && (
+						<ReactJson
+							className="json"
+							src={clanJson}
+							enableClipboard={false}
+							displayDataTypes={false}
+							indentWidth={2}
+						/>
+					)}
 				</div>
 			</div>
 		</div>
