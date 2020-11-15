6e3885fbe770c122fb87e70a625da060a323ef30
Tinotenda Muzambi <tinomuzambi@gmail.com>
2020-11-15 22:57:28 +0200

    Adds reference to my profile.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 			<div className="logo">
 				<h1>Working with Clash</h1>
 			</div>
+                        <h3>Paste L98JC2LG in the player tag to view my profile.</h3>
 			<div className="forms">
 				<Card content="player" />
 				<Card content="clan" />
