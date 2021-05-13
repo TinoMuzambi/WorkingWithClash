80210fed85caac08f5b585c5f96519c468223c8f
Tino Muzambi <tinomuzambi@gmail.com>
2021-05-13 11:43:42 +0200

    Uses higher order component to render common card content.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from "react";
-import "./css/App.min.css";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	useParams,
+} from "react-router-dom";
+
 import Card from "./components/Card";
+import "./css/App.min.css";
+
+const CommonCardContent = ({ children }) => (
+	<>
+		<div className="logo">
+			<h1>Working with Clash</h1>
+		</div>
+		<div className="info-wrapper">
+			<h3 className="info">
+				Paste L98JC2LG in the player info section to view my profile.
+			</h3>
+		</div>
+		<div className="forms">
+			{children}
+			{/* <Card content="clan" /> */}
+		</div>
+	</>
+);
 
 function App() {
 	return (
-		<>
-			<div className="logo">
-				<h1>Working with Clash</h1>
-			</div>
-			<div className="info-wrapper">
-				<h3 className="info">
-					Paste L98JC2LG in the player info section to view my profile.
-				</h3>
-			</div>
-
-			<div className="forms">
-				<Card content="player" />
-				{/* <Card content="clan" /> */}
-			</div>
-		</>
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<CommonCardContent>
+						<Card content="player" />
+					</CommonCardContent>
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
 
